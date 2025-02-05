f09f6c45bb38680204fa03db5d83b9234b640243
Ultrazoic <rajpalbchoudhary4287@gmail.com>
2025-02-05 23:09:20 +0530

    calculator

---
+++ 4.py
@@ -0,0 +1,8 @@
+#code to build a calculator using python 
+
+a=int(input("Enter the first number\n"))
+b=int(input("Enter the second number\n"))
+print("the add is :",a+b)
+print("the sub is :",a-b)
+print("the multiplication is :",a*b)
+print("the division is :",a/b)
