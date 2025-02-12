d80af5558a78e2e78bfb784daa47552d72b6d45d
Ultrazoic <rajpalbchoudhary4287@gmail.com>
2025-02-12 21:35:24 +0530

    kbc project

---
+++ KBCproject.py
@@ -0,0 +1,15 @@
+question=("when was maharana pratap born?")
+options=["A.15 dec      B.9 may    C.12 jan     D.23 april "]
+print(question)
+print(options)
+answer=input()
+
+if(answer=="B" or answer=="b"):
+    print("Congrats You won 20,000 Rs Want To play more or take the prize?\n Type 'yes' to play more \n or 'no' to stop ")
+    opinion=input()
+    if(opinion=="yes"):
+        print("Your Next Question is.....")
+    else:
+        print("Thanks For playing you can take your prize!!")
+else:
+    print("Oops!! Better Luck next time")
