6be142f1d6c64166e0b919b8d22ba62ac3db5823
Ultrazoic <rajpalbchoudhary4287@gmail.com>
2025-02-19 21:45:45 +0530

    input feature

--- KBCproject.py
+++ KBCproject.py
@@ -1,10 +1,11 @@
+# Python program for KBC Questions
 question=("when was maharana pratap born?")
-options=["A.15 dec      B.9 may    C.12 jan     D.23 april "]
+options="A.15 dec      B.9 may    C.12 jan     D.23 april "
 print(question)
 print(options)
-answer=input()
+answer=input().lower() #here the lower method is used to accept both lower and uppercase as input
 
-if(answer=="B" or answer=="b"):
+if(answer== "b" ):
     print("Congrats You won 20,000 Rs Want To play more or take the prize?\n Type 'yes' to play more \n or 'no' to stop ")
     opinion=input()
     if(opinion=="yes"):
